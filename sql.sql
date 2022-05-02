4fe9d06a92c78d6b242e8b1a2f1a50245af241bc
N3PH1L4X <taxlopez2000@hotmail.com>
2022-05-02 15:08:42 -0400

    sql modificado inserts

--- sql.sql
+++ sql.sql
@@ -16,4 +16,5 @@ CREATE TABLE productos (
     gramajeProducto VARCHAR(50) NOT NULL
 );
 
+INSERT INTO `users` (`username`, `password`) VALUES ('prueba','$2y$10$rQuCO0dCs7Dz6PtbU162r.S8qWh2VHHshX4LRBYYAmEd/lUMyXPFi')
 INSERT INTO `productos` (`idProducto`, `nombProducto`, `fabricProducto`, `descripProducto`, `cantidProducto`, `gramajeProducto`) VALUES (NULL, 'Paracetamol', 'FabricaFina', 'medicina pa la guata', '24', '500gr')
